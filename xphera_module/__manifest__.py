bb359d02ce0e7f0051845ee631d5bc2492c18667
Cristian Izaquita <cristian.izaquita@xphera.co>
2020-08-13 15:36:05 -0500

    Modificación de onchange para fecha en orden de cotización purchase.order. Manifest fix

--- xphera_module/__manifest__.py
+++ xphera_module/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -24,11 +24,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
