0e8be35c0f1a6b8089c1094dbc57e73300ebb2ed
Cristian Izaquita <cristian.izaquita@gmail.com>
2020-08-14 19:39:15 -0500

    Se modifica validación mensaje de error y ubicación de asignación de variable cantidad quantity

--- modulo_xphera/models/models.py
+++ modulo_xphera/models/models.py
@@ -81,18 +81,18 @@ class ReturnPickingLine(models.TransientModel):
         mensaje = ""
 
         if self.quantity and self.quantity > self.product_id.qty_available:
-            mensaje += "La cantidad a devolver no puede ser mayor a la cantidad en mano. Actual: {}".format(self.product_id.qty_available)
+            mensaje += "La cantidad a devolver no puede ser mayor a la cantidad A Mano (Inventario). Actual: {}".format(self.product_id.qty_available)
 
         if self.quantity and self.quantity > self.move_id.quantity_done:
             mensaje += "\nLa cantidad a devolver no puede ser mayor a la cantidad Terminado. Actual: {}".format(self.move_id.quantity_done)
 
         if len(mensaje) > 1:
-            self.quantity = 0
             warning = {
                 'title': "Error validación en la cantidad: {}".format(self.quantity),
                 'message': mensaje,
                 #'type': 'notification',
             }
+            self.quantity = 0
             res.update({'warning': warning})
         return res
 
