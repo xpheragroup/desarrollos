25e1e9b5ae1ae730bc4bdf7fd34cac1f5b558900
Cristian Izaquita <cristian.izaquita@gmail.com>
2020-09-02 17:42:03 -0500

    Se añaden opciones de accesos al módulo creado.

--- keralty_module/__manifest__.py
+++ keralty_module/__manifest__.py
@@ -24,8 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
