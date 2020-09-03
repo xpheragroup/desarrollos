aa6c3cefca10f666ff4d010da0aea86ae93feede
Cristian Izaquita <cristian.izaquita@gmail.com>
2020-09-02 21:18:13 -0500

    Fix: Se sobreescrib ía modelo por eso no me permitía añadir más, el sobrescrito solo tenia un puto campo.

--- keralty_module/models/models.py
+++ keralty_module/models/models.py
@@ -6,35 +6,28 @@ class FormularioParametrizacion(models.Model):
     _name = 'keralty_module.formulario.param'
     _description = 'Formulario Parametrización'
 
-    nombre_codigo = fields.Char(required=True, string="Nombre o Código", help="PRUEBA AYUDA")
-    nombre_codigo_ex = fields.Char(required=True, string="Nombre o Código", help="PRUEBA AYUDA")
-    nombre_codigo_ex_ex = fields.Char(required=True, string="Nombre o Código", help="PRUEBA AYUDA")
-    casilleros = fields.Char(string="Área de casilleros para funcionarios", required=True)
-    banios_hombres = fields.Char(string="Baños públicos hombres", required=True, help="Baños públicos hombres")
-    banios_mujeres = fields.Char(string="Baños públicos mujeres", required=True, help="Baños públicos mujeres")
-    banios_hombres_disc = fields.Char(string="Baños públicos hombres en condición de discapacidad.", required=True, help="Baños publícos hombres en condición de discapacidad")
-    banios_mujeres_disc = fields.Char(string="Baños públicos mujeres en condición de discapacidad", required=True, help="Baños públicos mujeres en condición de discapacidad")
-    cafeteria_empleados = fields.Char(string="Cafeteria empleados", required=True, help="Cafeteria empleados")
-    cuarto_aseo = fields.Char(string="Cuarto de aseo (poceta)", required=True, help="Cuarto de aseo (poceta)")
-    banios_hombres_emp = fields.Char(string="Baño hombres para empleados", required=True, help="Baño hombres para empleados")
-    banios_mujeres_emp = fields.Char(string="Baño mujeres para empleados", required=True, help="Baño mujeres para empleados")
+    nombre_codigo = fields.Char(required=True, string="Nombre o Código")
+    casilleros = fields.Float(string="Área de casilleros para funcionarios", required=True, help="Area de casilleros para funcionarios", digits=(8, 2))
+    banios_hombres = fields.Float(string="Baños públicos hombres", required=True, help="Baños públicos hombres", digits=(8, 2))
+    banios_mujeres = fields.Float(string="Baños públicos mujeres", required=True, help="Baños públicos mujeres", digits=(8, 2))
+    banios_hombres_disc = fields.Float(string="Baños públicos hombres en condición de discapacidad.", required=True, help="Baños publícos hombres en condición de discapacidad", digits=(8, 2))
+    banios_mujeres_disc = fields.Float(string="Baños públicos mujeres en condición de discapacidad", required=True, help="Baños públicos mujeres en condición de discapacidad", digits=(8, 2))
+    cafeteria_empleados = fields.Float(string="Cafeteria empleados", required=True, help="Cafeteria empleados", digits=(8, 2))
+    cuarto_aseo = fields.Float(string="Cuarto de aseo (poceta)", required=True, help="Cuarto de aseo (poceta)", digits=(8, 2))
+    banios_hombres_emp = fields.Float(string="Baño hombres para empleados", required=True, help="Baño hombres para empleados", digits=(8, 2))
+    banios_mujeres_emp = fields.Float(string="Baño mujeres para empleados", required=True, help="Baño mujeres para empleados", digits=(8, 2))
     # Laboratorio
-    banios_hombres_lab = fields.Char(string="Baños públicos hombres laboratorio", required=True, help="Baños públicos hombres laboratorio")
-    banios_mujeres_lab = fields.Char(string="Baños públicos mujeres laboratorio", required=True, help="Baños públicos mujeres laboratorio")
-    banios_hombres_lab_disc = fields.Char(string="Baños públicos hombres en condición de discapacidad laboratorio", required=True, help="Baños públicos hombres en condición de discapacidad laboratorio")
-    banios_mujeres_lab_disc = fields.Char(string="Baños públicos mujeres en condición de discapacidad laboratorio", required=True, help="Baños públicos mujeres en condición de discapacidad laboratorio")
-    banio_mixto = fields.Char(string="Baño mixto para empleados", required=True, help="Baño mixto para empleados")
-    vestier = fields.Char(string="Vestier con casilleros mixto para empleados", required=True, help="Vestier con casilleros mixto para empleados")
-    oficina_admon = fields.Char(string="Oficina coordinación adminsitrativa", required=True, help="Oficina coordinación adminsitrativa")
+    banios_hombres_lab = fields.Float(string="Baños públicos hombres laboratorio", required=True, help="Baños públicos hombres laboratorio", digits=(8, 2))
+    banios_mujeres_lab = fields.Float(string="Baños públicos mujeres laboratorio", required=True, help="Baños públicos mujeres laboratorio", digits=(8, 2))
+    banios_hombres_lab_disc = fields.Float(string="Baños públicos hombres en condición de discapacidad laboratorio", required=True, help="Baños públicos hombres en condición de discapacidad laboratorio", digits=(8, 2))
+    banios_mujeres_lab_disc = fields.Float(string="Baños públicos mujeres en condición de discapacidad laboratorio", required=True, help="Baños públicos mujeres en condición de discapacidad laboratorio", digits=(8, 2))
+    banio_mixto = fields.Float(string="Baño mixto para empleados", required=True, help="Baño mixto para empleados", digits=(8, 2))
+    vestier = fields.Float(string="Vestier con casilleros mixto para empleados", required=True, help="Vestier con casilleros mixto para empleados", digits=(8, 2))
+    oficina_admon = fields.Float(string="Oficina coordinación adminsitrativa", required=True, help="Oficina coordinación adminsitrativa", digits=(8, 2))
 
     #     value = fields.Integer()
 
 
-class FormularioParametrizacion(models.Model):
-    _name = 'keralty_module.formulario.param'
-    _description = 'Formulario Parametrización'
-
-    nombre_codigo = fields.Char(required=True)
 # class keralty_module(models.Model):
 #     _name = 'keralty_module.keralty_module'
 #     _description = 'keralty_module.keralty_module'
