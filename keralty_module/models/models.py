51621c36f572dbad3d358062e475b983cd2a9fbe
Cristian Izaquita <cristian.izaquita@gmail.com>
2020-10-10 09:41:22 -0500

    Se crea modelo prueba asociación muchos a uno formulario de cliente a formulario de valdiación técnica.

--- keralty_module/models/models.py
+++ keralty_module/models/models.py
@@ -43,6 +43,17 @@ class FormularioCliente(models.Model):
     consultorio_general_con_banio_disc = fields.Float(string="Consultorio General con baño en condición de discapacidad", required=True, help="Consultorio General con baño en condición de discapacidad", digits=(8, 2))
     consultorio_general_crecimiento_desarrollo = fields.Float(string="Consultorio General crecimiento y desarrollo", required=True, help="Consultorio General crecimiento y desarrollo", digits=(8, 2))
     consultorio_vacunacion = fields.Float(string="Consultorio Vacunación", required=True, help="Consultorio Vacunación", digits=(8, 2))
+    areas_categoria = fields.Many
+
+class FormularioValidacion(models.Model):
+    _name = 'keralty_module/.formulario.validacion'
+    _descripción = 'Formulario Validación'
+
+    nombre_tecnico = fields.Char(required=True, string="Nombre Proyecto")
+    descripcion = fields.Char(required=True, string="Descripcion")
+    formulario_cliente = fields.Many2one(string="Formulario Cliente", comodel_name='keralty_module.formulario.cliente',
+                    help="Formulario Cliente asociado para validación técnica.")
+
 
 # class keralty_module(models.Model):
 #     _name = 'keralty_module.keralty_module'
